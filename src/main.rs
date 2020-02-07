32573dd9237a23306e9cf2300d53161b7e6c7e4f
Zac Joffe <zacharyjoffe@gmail.com>
2020-02-06 19:17:31 -0500

    Basic cpu struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Cpu {
+    a: u8,
+    x: u8,
+    y: u8,
+    pc: u16,
+    sp: u8,
+    p: u8,
+
+    opcode_table[256; fn(&mut self)]
+}
+
+impl Cpu {
+    fn new() -> Cpu {
+        a: 0,
+        x: 0,
+        y: 0,
+        pc: 0,
+        sp: 0,
+        p: 0,
+
+        opcode_table: self.generate_opcode_table()
+    }
+
+    fn generate_opcode_tabe(&self) -> [256; fn(&mut self)] {
+
+    }
+}
