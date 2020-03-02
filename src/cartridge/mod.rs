2f75b808516a561982ad3a8828faeaae3f7eb411
Zac Joffe <zacharyjoffe@gmail.com>
2020-03-01 20:24:51 -0500

    Cartridge constructor

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -1,9 +1,25 @@
 struct Cartridge {
     prg: Vec<u8>,
     chr: Vec<u8>,
-    sram: Vec<u8>,
 
     mapper: u8,
     mirror: u8,
     battery: u8
+}
+
+impl Cartridge {
+    pub fn new(prg: Vec<u8>, chr: Vec<u8>, mapper: u8, mirror: u8, battery: u8) -> Cartridge {
+        Cartridge {
+            prg: prg,
+            chr: chr,
+            mapper: mapper,
+            mirror: mirror,
+            battery: battery
+        }
+    }
+}
+
+// https://wiki.nesdev.com/w/index.php/INES
+struct NesFileHeader {
+    signature: u32,
 }
