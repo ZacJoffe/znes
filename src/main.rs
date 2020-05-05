4c0a4fc6277f4f5dc9f30dc6b991f2ba1c6b5340
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-05 19:54:56 -0400

    SDL2 driver code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+extern crate sdl2;
+
 mod cpu;
 mod cartridge;
 mod ppu;
 mod controller;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 use cpu::CPU;
 use ppu::PPU;
@@ -27,6 +32,22 @@ fn main() {
     };
 
     println!("{:x?}", buffer);
+
+    // initialize sdl2
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("znes", 256, 240).position_centered().build().unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let texture_creator = canvas.texture_creator();
+
+    let mut texture = texture_creator.create_texture_streaming(sdl2::pixels::PixelFormatEnum::RGB24, 256, 240).unwrap();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mapper = get_mapper(buffer);
     let ppu = PPU::new(mapper.clone());
