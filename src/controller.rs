c8fae92045c83b356dc9b47c2150c05c4bfd2f6a
Zac Joffe <zacharyjoffe@gmail.com>
2020-05-05 17:14:17 -0400

    Fix read logic and documentation

--- src/controller.rs
+++ src/controller.rs
@@ -1,7 +1,7 @@
 pub struct Controller {
-    buttons: u8,
-    index: u8,
-    strobe: u8
+    buttons: u8, // each index represents an input
+    index: u8, // index that is being looked at
+    strobe: u8 // button write
 }
 
 impl Controller {
@@ -14,9 +14,11 @@ impl Controller {
     }
 
     pub fn read(&mut self) -> u8 {
-        let value = if self.index < 8 && (self.buttons & (1 << self.index) != 0) { 0 } else { 1 };
+        // we read the bit at the index and return its value
+        let value = if self.index < 8 && (self.buttons & (1 << self.index) != 0) { 1 } else { 0 };
         self.index += 1;
 
+        // when the first bit in the strobe is high, reading will return the current state of the A button
         if self.strobe & 1 != 0 {
             self.index = 0;
         }
@@ -25,6 +27,7 @@ impl Controller {
     }
 
     pub fn write(&mut self, value: u8) {
+        // write the value to the strobe
         self.strobe = value;
 
         if self.strobe & 1 == 1 {
